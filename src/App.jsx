b97958c8afaf2640de693b9eed90e0509d265284
Mark S. Shenouda <mark.s.shenouda@gmail.com>
2022-11-11 09:54:12 +0200

    vr support

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { Suspense } from "react";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import World from "@/components/World";
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
 
 function App() {
   return (
-    <Canvas style={{ position: "absolute" }}>
-      <Suspense fallback={null}>
-        <OrbitControls />
-        <World />
-      </Suspense>
-    </Canvas>
+    <>
+      <VRButton />
+      <Canvas style={{ position: "absolute" }}>
+        <Suspense fallback={null}>
+          <OrbitControls />
+          <XR>
+            <Controllers />
+            <Hands />
+            <World />
+          </XR>
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
 
